68ca0cb31618c7cdccda17a60346d5ccad10d6d4
LoisClinton <loisclinton@hotmail.co.uk>
2024-09-25 17:12:45 +0100

    Create Admin and Customer Users on start up

--- ECommerceApplication/src/main/java/com/SportyShoes/ECommerceApplication/ECommerceApplication.java
+++ ECommerceApplication/src/main/java/com/SportyShoes/ECommerceApplication/ECommerceApplication.java
@@ -1,13 +1,55 @@
 package com.SportyShoes.ECommerceApplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.SportyShoes.ECommerceApplication.model.User;
+import com.SportyShoes.ECommerceApplication.repository.UserRepository;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ECommerceApplication {
 
+	@Autowired
+	private UserRepository userRepository;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
 		System.out.println("E-Commerce Application Started!!");
+	}
+
+	@PostConstruct
+	public void initAdminUser() {
+		// Check if admin already exists
+		if (userRepository.findByEmail("admin@gmail.com") == null) {
+			// Create a new admin user
+			User adminUser = new User();
+			adminUser.setEmail("admin@gmail.com");
+			adminUser.setPassword("admin@123");
+			adminUser.setUserType("admin"); // Set user type as 'admin'
+
+			// Save admin user
+			userRepository.save(adminUser);
+			System.out.println("Admin user created successfully!");
+		}
+	}
+
+	@PostConstruct
+	public void initCustomerUser() {
+		// Check if customer already exists
+		if (userRepository.findByEmail("customer@gmail.com") == null) {
+			// Create a new customer user
+			User standardUser = new User();
+			standardUser.setEmail("customer@gmail.com");
+			standardUser.setPassword("customer@123");
+			standardUser.setUserType("customer"); // Set user type as 'customer'
+
+			// Save customer user
+			userRepository.save(standardUser);
+			System.out.println("Customer user created successfully!");
+		}
 	}
 }
