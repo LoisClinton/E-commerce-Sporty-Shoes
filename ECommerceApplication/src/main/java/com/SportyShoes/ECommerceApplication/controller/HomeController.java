6dfc8a918a797cc3f61942ee3fd3ee08b4c29709
LoisClinton <loisclinton@hotmail.co.uk>
2024-09-30 10:32:31 +0100

    add customer shop, new pages and add functionality in Home Controller

--- ECommerceApplication/src/main/java/com/SportyShoes/ECommerceApplication/controller/HomeController.java
+++ ECommerceApplication/src/main/java/com/SportyShoes/ECommerceApplication/controller/HomeController.java
@@ -1,15 +1,31 @@
 package com.SportyShoes.ECommerceApplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.SportyShoes.ECommerceApplication.model.Product;
+import com.SportyShoes.ECommerceApplication.repository.ProductRepository;
 
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/home")
-    public String loginPage() {
-        return "home"; // returns login.html page
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public String homePage(Model model) {
+        // Give Thymeleaf model empty product object for form binding
+        model.addAttribute("product", new Product());
+        model.addAttribute("p", new Product());
+        //Give Thymeleaf model all products to display in the table
+        model.addAttribute("products", productRepository.findAll());
+
+        return "home"; // returns home.html page
     }
 }
